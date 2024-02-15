5bad30b643ac9b3f5264bb14051b4fb8267be56b
Deyruby <128061593+Deyruby@users.noreply.github.com>
2024-02-15 21:28:17 +0000

    avances

--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 
+
 import { Context } from "../store/appContext";
 
 import "../../styles/demo.css";
@@ -9,35 +10,36 @@ export const Demo = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="container">
-			<ul className="list-group">
-				{store.demo.map((item, index) => {
-					return (
-						<li
-							key={index}
-							className="list-group-item d-flex justify-content-between"
-							style={{ background: item.background }}>
-							<Link to={"/single/" + index}>
-								<span>Link to: {item.title}</span>
-							</Link>
-							{// Conditional render example
-							// Check to see if the background is orange, if so, display the message
-							item.background === "orange" ? (
-								<p style={{ color: item.initial }}>
-									Check store/flux.js scroll to the actions to see the code
-								</p>
-							) : null}
-							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
-								Change Color
-							</button>
-						</li>
-					);
-				})}
-			</ul>
-			<br />
-			<Link to="/">
-				<button className="btn btn-primary">Back home</button>
-			</Link>
-		</div>
-	);
+	<>
+	<div className="container">
+	<form>
+  <div className="mb-3">
+	<h1 className="text-center">Add a New Contact</h1>
+    <label for="exampleInputEmail1" className="form-label">Full name</label>
+    <input type="name" className="form-control" placeholder= "Full name" id="exampleInputName" aria-describedby="NamelHelp"/>
+  </div>
+  <div className="mb-3">
+    <label for="exampleInputEmail" className="form-label">Email</label>
+    <input type="email" className="form-control" placeholder= "Email" id="exampleInputEmail"/>
+  </div>
+  <div className="mb-3">
+    <label for="exampleInputPhone" className="form-label">Phone</label>
+    <input type="phone" className="form-control" placeholder= "Phone"id="exampleInputPhone"/>
+  </div>
+  <div className="mb-3">
+    <label for="address" className="form-label">Address</label>
+    <input type="password" className="form-control" placeholder= "Address" id="exampleInputAddress"/>
+  </div>
+  <div class="d-grid gap-2">
+  <button class="btn btn-primary" type="button">Save</button>
+  <Link to= "/">
+ <a href="#" className="text-decoration-none">or get back to contacts</a>
+  </Link>
+</div>
+</form>
+</div>
+	</>					
+	)					
+		
+	
 };
